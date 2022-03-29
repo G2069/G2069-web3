ee6d79fe8d2e271a62cf69b56bafa2a8249472cc
G2069 <admin@g2069.com>
2022-03-29 13:15:24 +0800

    preseedcontract v1.0.1

--- g2069-web3/src/utils/constants.js
+++ g2069-web3/src/utils/constants.js
@@ -5,4 +5,4 @@ export const tokenAbi = TokenAbi.abi;
 export const tokenAddress = "0xaaa47647C34C446921940E89D012dC93aC876606";
 
 export const preSeedAbi = PreSeedAbi.abi;
-export const preSeedAddress = "0x035d7795338ede85a38B930a14f83E8Dc2C37836";
+export const preSeedAddress = "0x77776039e784e630EbF2B3Df302Ce49aC293836C";
